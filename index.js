1a774aa4c286a090f5589d7e38bcd447286a1893
DevenPerkins <deven.s.perkins@gmail.com>
2022-06-30 23:45:18 -0700

    services, entities, routes, and factories

--- index.js
+++
@@ -1,9 +0,0 @@
-import http from "node:http";
-import handler from "./handler.js";
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer(handler).listen(PORT, () => {
-  console.log(`Server is running at ${PORT}`);
-});
-
-export { server };
--- src/handler.js
+++ src/handler.js
@@ -1,36 +1,46 @@
-import { parse } from "node:url";
-import { DEFAULT_HEADER } from "./util/util.js";
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { parse } from 'node:url';
+import { routes } from './routes/stockRoute.js';
+import { DEFAULT_HEADER } from './util/util.js';
+
+import { generateInstance } from './factories/stockFactory.js';
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+const filepath = join(currentDir, './../database', '/data.json');
+
+const stockService = generateInstance({ filepath });
+const stockRoutes = routes({ stockService });
+
 const allRoutes = {
-  "/stocks:get": (request, response) => {
-    response.write("GET");
-    response.end();
-  },
-  // 404 routes
-  default: (request, response) => {
-    response.writeHead(404, DEFAULT_HEADER);
-    response.write("(╯°□°)╯︵ ┻━┻ punoɟ ʇou ㄣ0ㄣ");
-    response.end();
-  },
+    ...stockRoutes,
+    // 404 routes
+    default: (request, response) => {
+        response.writeHead(404, DEFAULT_HEADER);
+        response.write('(╯°□°)╯︵ ┻━┻ punoɟ ʇou ㄣ0ㄣ');
+        response.end();
+    },
 };
 function handler(request, response) {
-  const { url, method } = request;
-  const { pathname } = parse(url, true);
+    const { url, method } = request;
+    const { pathname } = parse(url, true);
 
-  const key = `${pathname}:${method.toLowerCase()}`;
-  const chosen = allRoutes[key] || allRoutes.default;
+    const key = `${pathname}:${method.toLowerCase()}`;
+    const chosen = allRoutes[key] || allRoutes.default;
 
-  return Promise.resolve(chosen(request, response)).catch(
-    handlerError(response)
-  );
+    return Promise.resolve(chosen(request, response)).catch(
+        handlerError(response),
+    );
 }
 
 function handlerError(response) {
-  return (error) => {
-    console.log("Something has gone terribly wrong", error.stack);
-    response.writeHead(500, DEFAULT_HEADER);
-    response.write(JSON.stringify({ error: "internet server error" }));
+    return error => {
+        console.log('Something has gone terribly wrong', error.stack);
+        response.writeHead(500, DEFAULT_HEADER);
+        response.write(JSON.stringify({ error: 'internet server error' }));
 
-    return response.end();
-  };
+        return response.end();
+    };
 }
 export default handler;
